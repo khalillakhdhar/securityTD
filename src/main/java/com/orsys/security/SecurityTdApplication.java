1facca264e3fbf4b95c0e7f6809ac8f49c7acf71
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-10 12:54:34 +0100

    Initial commit

---
+++ src/main/java/com/orsys/security/SecurityTdApplication.java
@@ -0,0 +1,13 @@
+package com.orsys.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTdApplication.class, args);
+	}
+
+}
